ce79813e1cf4277f423bc6a1569e767f7a00b76b
Prasad Jaware <99593453+Prasad4742@users.noreply.github.com>
2023-02-02 09:26:47 +0530

    Update Using DP.c

--- Problem 1/Using DP.c
+++ Problem 1/Using DP.c
@@ -1,56 +1,73 @@
-typedef struct Node
-{
-    TypeTag type;
-    int value;
-    struct Node *left;
-    struct Node *right;
-} Node;
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
 
-typedef enum TypeTag {
-    ADD,
-    SUB,
-    MUL,
-    DIV,
-    FIB,
-    CONST
-} TypeTag;
-
-Node* makeFunc(TypeTag type) {
-    Node *node = (Node *) malloc(sizeof(Node));
-    node->type = type;
-    return node;
+//Function F gives nth fibonacci number in output recurssively
+int F(int n) {
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+    if (n == 2) return 2;
+    return F(n-1) + F(n-2);
 }
 
-int calc(Node *node) {
-    if(node->type == ADD) {
-        return calc(node->left) + calc(node->right);
-    } else if(node->type == SUB) {
-        return calc(node->left) - calc(node->right);
-    } else if(node->type == MUL) {
-        return calc(node->left) * calc(node->right);
-    } else if(node->type == DIV) {
-        return calc(node->left) / calc(node->right);
-    } else if(node->type == FIB) {
-        if(node->left == NULL) {
-            return 0;
-        } else if(node->left->value == 0) {
-            return 0;
-        } else if(node->left->value == 1) {
-            return 1;
-        } else {
-            Node *left = makeFunc(FIB);
-            left->left = (Node *) malloc(sizeof(Node));
-            left->left->value = node->left->value - 1;
-            Node *right = makeFunc(FIB);
-            right->left = (Node *) malloc(sizeof(Node));
-            right->left->value = node->left->value - 2;
-            Node *add = makeFunc(ADD);
-            add->left = left;
-            add->right = right;
-            return calc(add);
-        }
-    } else {
-        return node->value;
+struct Node {
+    int data;
+    Node* next;
+};
+
+Node* addNumbers(int a, int b) {
+    Node* result = new Node;
+    result->data = a + b;
+    result->next = nullptr;
+    return result;
+}
+
+Node* mulNumbers(int a, int b) {
+    Node* result = new Node;
+    result->data = a * b;
+    result->next = nullptr;
+    return result;
+}
+
+Node* subNumbers(int a,int b) {
+    Node* result = new Node;
+    result->data = a - b;
+    result->next = nullptr;
+    return result;
+}
+
+
+
+Node* (*makeFunc(char op))(int, int) {
+    switch (op) {
+        case '+':
+            return addNumbers;
+        case '*':
+            return mulNumbers;
+        case '-':
+            return subNumbers;
+        default:
+            return nullptr;
     }
 }
 
+int main() {
+    Node* add = (*makeFunc('+'))(10, 6);
+    Node* mul = (*makeFunc('*'))(5, 4);
+    //conversion to integer
+    int addResult = add->data; int mulResult = mul->data; 
+    
+    Node* sub = (*makeFunc('-'))(addResult, mulResult);
+    //conversion to integer
+    int subResult = sub->data;
+    
+    Node *fibo = (*makeFunc('-'))(abs(subResult), NULL);
+    //conversion to integer
+    int n = fibo->data;
+    
+    std::cout << add->data << std::endl;
+    std::cout << mul->data << std::endl;
+    std::cout << sub->data << std::endl;
+    std::cout << F(n) << std::endl;
+    return 0;
+}
